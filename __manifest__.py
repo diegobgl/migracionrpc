c68801322336b763c7d4a73ec36eb64e36b0d459
Diego Gajardo <diegobgajardo@gmail.com>
2025-05-27 13:14:59 -0400

    .7.1

--- __manifest__.py
+++ __manifest__.py
@@ -21,7 +21,7 @@
          'security/ir.model.access.csv',
         'views/migration_views.xml',
         'views/lot_view.xml',
-        'views/migration_menu.xml'
+        'views/migration_menu.xml',
         'views/server_action.xml',
     ],
     'demo': [
